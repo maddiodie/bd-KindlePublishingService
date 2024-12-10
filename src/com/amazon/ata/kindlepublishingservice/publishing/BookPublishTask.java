68eeffa169026e9c7316621759f1cbcc2a025aea
maddiodie <maddiodie@gmail.com>
2024-12-09 23:46:19 -0700

    added some logging code to BookPublishTask in order to figure out the error ... my next move is to run this test at home on a better network ... <3

--- src/com/amazon/ata/kindlepublishingservice/publishing/BookPublishTask.java
+++ src/com/amazon/ata/kindlepublishingservice/publishing/BookPublishTask.java
@@ -3,10 +3,13 @@ package com.amazon.ata.kindlepublishingservice.publishing;
 import com.amazon.ata.kindlepublishingservice.dao.CatalogDao;
 import com.amazon.ata.kindlepublishingservice.dao.PublishingStatusDao;
 import com.amazon.ata.kindlepublishingservice.enums.PublishingRecordStatus;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 
 public class BookPublishTask implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookPublishTask.class);
 
     private BookPublishRequestManager bookPublishRequestManager;
     private PublishingStatusDao publishingStatusDao;
@@ -26,21 +29,28 @@ public class BookPublishTask implements Runnable {
                 bookPublishRequestManager.getBookPublishRequestToProcess();
 
         if (bookPublishRequestManager == null) {
+            logger.info("No book publish requests to process.");
             return;
         }
         // if there are no requests to process, return immediately ...
 
         try {
+            logger.info("Updating status to IN_PROGRESS for publishing record ID.");
             publishingStatusDao.setPublishingStatus(bookPublishRequest.getPublishingRecordId(),
                     PublishingRecordStatus.IN_PROGRESS, bookPublishRequest.getBookId());
+            // updates status to IN_PROGRESS
 
             processBookRequest(bookPublishRequest);
-            
+
+            logger.info("Updating status to SUCCESSFUL for publishing record ID.");
             publishingStatusDao.setPublishingStatus(bookPublishRequest.getPublishingRecordId(),
                     PublishingRecordStatus.SUCCESSFUL, bookPublishRequest.getBookId());
+            // updated status to SUCCESSFUL
         } catch (Exception e) {
+            logger.error("Failed to process book publish request for publishing record ID.");
             publishingStatusDao.setPublishingStatus(bookPublishRequest.getPublishingRecordId(),
                     PublishingRecordStatus.FAILED, bookPublishRequest.getBookId(), e.getMessage());
+            // update status to FAILED in case of an exception
             e.printStackTrace();
         }
 
